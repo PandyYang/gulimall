1bd488433b65fb84ff80eb93f8fdedd8fe165b0c
PandyYang <yangpandy@gmail.com>
2021-08-22 12:16:11 +0800

    mybatis分页插件

---
+++ gulimall-product/src/main/java/com/pandy/gulimall/product/config/MybatisConfig.java
@@ -0,0 +1,26 @@
+package com.pandy.gulimall.product.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @Author Pandy
+ * @Date 2021/8/21 20:20
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.pandy.gulimall.product.dao")
+public class MybatisConfig {
+
+    // 引入分页插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setOverflow(true);
+        paginationInterceptor.setLimit(1000);
+        return paginationInterceptor;
+    }
+}
