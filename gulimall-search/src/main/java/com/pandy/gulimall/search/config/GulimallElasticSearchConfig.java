cab4b8c75b53e1ca6ecd9905562140a3cab7146f
PandyYang <yangpandy@gmail.com>
2021-08-29 01:11:37 +0800

    es单元测试

--- gulimall-search/src/main/java/com/pandy/gulimall/search/config/GulimallElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/pandy/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -1,6 +1,8 @@
 package com.pandy.gulimall.search.config;
 
 import org.apache.http.HttpHost;
+import org.bouncycastle.cert.ocsp.Req;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +15,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GulimallElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        COMMON_OPTIONS = builder.build();
+    }
 
     @Bean
     public RestHighLevelClient esRestClient() {
--- gulimall-search/src/test/java/com/pandy/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/pandy/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,11 +1,19 @@
 package com.pandy.gulimall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.pandy.gulimall.search.config.GulimallElasticSearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,9 +27,29 @@ public class GulimallSearchApplicationTests {
         System.out.println(restHighLevelClient);
     }
 
+
+    @Data
+    class User {
+        private String username;
+        private String gender;
+        private Integer age;
+    }
+
     @Test
-    public void test() {
-        System.out.println(123);
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+//        indexRequest.source("username","zhangsan","age",18);
+        User user = new User();
+        user.username = "123";
+        user.gender = "123";
+        user.age = 12;
+        String s = JSON.toJSONString(user);
+        indexRequest.source(s, XContentType.JSON);
+
+        IndexResponse index = restHighLevelClient.index(indexRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+
+        System.out.println("index = " + index);
     }
 
 }
