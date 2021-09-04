783639585ca52460602961892eb765802f4cecbb
PandyYang <yangpandy@gmail.com>
2021-09-04 19:25:14 +0800

    使用redisson实现读写锁，分布式一致性

--- gulimall-product/src/main/java/com/pandy/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/pandy/gulimall/product/web/IndexController.java
@@ -3,14 +3,22 @@ package com.pandy.gulimall.product.web;
 import com.pandy.gulimall.product.entity.CategoryEntity;
 import com.pandy.gulimall.product.service.CategoryService;
 import com.pandy.gulimall.product.vo.Catelog2Vo;
+import lombok.SneakyThrows;
+import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>Title: IndexController</p>
@@ -23,123 +31,11 @@ public class IndexController {
 	@Autowired
 	private CategoryService categoryService;
 
-//	@Resource
-//	private RedissonClient redissonClient;
-//
-//	@Resource
-//	private StringRedisTemplate stringRedisTemplate;
+	@Autowired
+	RedissonClient redissonClient;
 
-	/**
-	 * RLock锁有看门狗机制 会自动帮我们续期，默认三秒自动过期
-	 * lock.lock(10,TimeUnit.SECONDS); 十二猴子的时间一定要大于业务的时间 否则会出现死锁的情况
-	 * <p>
-	 * 如果我们传递了锁的超时时间就给redis发送超时脚本 默认超时时间就是我们指定的
-	 * 如果我们未指定，就使用 30 * 1000 [LockWatchdogTimeout]
-	 * 只要占锁成功 就会启动一个定时任务 任务就是重新给锁设置过期时间 这个时间还是 [LockWatchdogTimeout] 的时间 1/3 看门狗的时间续期一次 续成满时间
-	 */
-//	@ResponseBody
-//	@RequestMapping("/index/hello")
-//	public String hello() {
-//		RLock lock = redissonClient.getLock("my-lock");
-//		// 阻塞式等待
-//		lock.lock();
-//		try {
-//			Thread.sleep(2000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		} finally {
-//			lock.unlock();
-//		}
-//		return "hello";
-//	}
-//
-//	/**
-//	 * 读写锁
-//	 */
-//	@GetMapping("/index/write")
-//	@ResponseBody
-//	public String writeValue() {
-//		RReadWriteLock lock = redissonClient.getReadWriteLock("rw-lock");
-//		RLock rLock = lock.writeLock();
-//		String s = "";
-//		try {
-//			rLock.lock();
-//			s = UUID.randomUUID().toString();
-//			Thread.sleep(3000);
-//			stringRedisTemplate.opsForValue().set("writeValue", s);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			rLock.unlock();
-//		}
-//		return s;
-//	}
-//
-//	/**
-//	 * 闭锁 只有设定的人全通过才关门
-//	 */
-//	@ResponseBody
-//	@GetMapping("/index/lockDoor")
-//	public String lockDoor() throws InterruptedException {
-//		RCountDownLatch door = redissonClient.getCountDownLatch("door");
-//		// 设置这里有5个人
-//		door.trySetCount(5);
-//		door.await();
-//
-//		return "5个人全部通过了...";
-//	}
-//
-//	@ResponseBody
-//	@GetMapping("/index/go/{id}")
-//	public String go(@PathVariable("id") Long id) throws InterruptedException {
-//
-//		RCountDownLatch door = redissonClient.getCountDownLatch("door");
-//		// 每访问一次相当于出去一个人
-//		door.countDown();
-//		return id + "走了";
-//	}
-//
-//	/**
-//	 * 尝试获取车位 [信号量]
-//	 * 信号量:也可以用作限流
-//	 */
-//	@ResponseBody
-//	@GetMapping("/index/park")
-//	public String park() {
-//
-//		RSemaphore park = redissonClient.getSemaphore("park");
-//		boolean acquire = park.tryAcquire();
-//		return "获取车位 =>" + acquire;
-//	}
-//
-//	/**
-//	 * 尝试获取车位
-//	 */
-//	@ResponseBody
-//	@GetMapping("/index/go/park")
-//	public String goPark() {
-//
-//		RSemaphore park = redissonClient.getSemaphore("park");
-//		park.release();
-//		return "ok => 车位+1";
-//	}
-//
-//	@GetMapping("/index/read")
-//	@ResponseBody
-//	public String readValue() {
-//		RReadWriteLock lock = redissonClient.getReadWriteLock("rw-lock");
-//		RLock rLock = lock.readLock();
-//		String s = "";
-//		rLock.lock();
-//		try {
-//			s = stringRedisTemplate.opsForValue().get("writeValue");
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			rLock.unlock();
-//		}
-//		return s;
-//	}
+	@Autowired
+	StringRedisTemplate redisTemplate;
 
 	@RequestMapping({"/", "index", "/index.html"})
 	public String indexPage(Model model) {
@@ -149,11 +45,86 @@ public class IndexController {
 		return "index";
 	}
 
+	@SneakyThrows
 	@ResponseBody
 	@RequestMapping("index/json/catalog.json")
-	public Map<String, List<Catelog2Vo>> getCatlogJson() {
+	public Map<String, List<Catelog2Vo>> getCatlogJson() throws InterruptedException {
 
-		Map<String, List<Catelog2Vo>> map = categoryService.getCatelogJson();
+		Map<String, List<Catelog2Vo>> map = null;
+		try {
+			map = categoryService.getCatelogJson();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return map;
+	}
+
+	@ResponseBody
+	@GetMapping("/hello")
+	public String hello() {
+		// 调用getlock 获取一把锁 锁的名字一样 就是同一把锁
+		RLock lock = redissonClient.getLock("my-lock");
+
+		// 加锁
+		lock.lock(10, TimeUnit.SECONDS);
+		try {
+			System.out.println("加锁成功执行业务, 线程为" + Thread.currentThread().getName());
+			Thread.sleep(30000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			System.out.println("释放锁:" + Thread.currentThread().getName());
+			lock.unlock();
+		}
+		return "hello";
+	}
+
+	/**
+	 * 保证一定能读到最新的数据
+	 * 修改期间
+	 * 写锁是一个排他锁、读锁是一个共享锁
+	 * 只要写锁没有释放 读就必须等待
+	 *
+	 * @return
+	 */
+	@GetMapping("write")
+	@ResponseBody
+	public String writeValue() {
+
+		RReadWriteLock readWriteLock = redissonClient.getReadWriteLock("rw-lock");
+		RLock wlock = readWriteLock.writeLock();
+
+		String s = "";
+		try {
+			//改数据加写锁 读数据加读锁
+			wlock.lock();
+			s = UUID.randomUUID().toString();
+			Thread.sleep(30000);
+			redisTemplate.opsForValue().set("writeValue", s);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}finally {
+			wlock.unlock();
+		}
+
+		return s;
+	}
+
+	@GetMapping("read")
+	@ResponseBody
+	public String readValue() {
+
+		RReadWriteLock readWriteLock = redissonClient.getReadWriteLock("rw-lock");
+		RLock rLock = readWriteLock.readLock();
+		String s = "";
+		rLock.lock();
+		try {
+			s = redisTemplate.opsForValue().get("writeValue");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			rLock.unlock();
+		}
+		return s;
 	}
 }
