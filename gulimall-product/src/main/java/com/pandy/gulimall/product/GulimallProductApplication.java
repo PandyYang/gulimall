7d06fb15dbf20ba94c5669a191abaa5e2bef6c0f
Pandy <yangpandy@gmail.com>
2021-09-21 12:35:57 +0800

    分布式session

--- gulimall-product/src/main/java/com/pandy/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/pandy/gulimall/product/GulimallProductApplication.java
@@ -1,19 +1,21 @@
 package com.pandy.gulimall.product;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.redisson.spring.session.config.EnableRedissonHttpSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableRedissonHttpSession
 @EnableCaching
 @MapperScan("com.pandy.gulimall.product.dao")
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableAsync
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
---
+++ gulimall-product/src/main/java/com/pandy/gulimall/product/config/GulimallSessionConfig.java
@@ -0,0 +1,34 @@
+package com.pandy.gulimall.product.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/**
+ * @Author Pandy
+ * @Date 2021/9/21 12:10
+ */
+@Configuration
+public class GulimallSessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer(){
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+        // 明确的指定Cookie的作用域
+        cookieSerializer.setDomainName("gulimall.com");
+        cookieSerializer.setCookieName("FIRESESSION");
+        return cookieSerializer;
+    }
+
+    /**
+     * 自定义序列化机制
+     * 这里方法名必须是：springSessionDefaultRedisSerializer
+     */
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer(){
+        return new GenericJackson2JsonRedisSerializer();
+    }
+}
