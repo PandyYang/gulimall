687743e3fec925ef3b445b2d964c4313aa041c33
Pandy <yangpandy@gmail.com>
2021-09-27 10:02:13 +0800

    rabbit 交换机 队列 绑定关系创建

--- gulimall-order/src/main/java/com/pandy/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/pandy/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,10 @@
 package com.pandy.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableRabbit
 @SpringBootApplication
 public class GulimallOrderApplication {
 
@@ -12,3 +13,4 @@ public class GulimallOrderApplication {
     }
 
 }
+
--- gulimall-order/src/test/java/com/pandy/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/pandy/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,46 @@
 package com.pandy.gulimall.order;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @SpringBootTest
-class GulimallOrderApplicationTests {
+@RunWith(SpringRunner.class)
+public class GulimallOrderApplicationTests {
+
+    @Autowired
+    AmqpAdmin amqpAdmin;
 
     @Test
-    void contextLoads() {
+    public void createExchange() {
+
+        DirectExchange directExchange = new DirectExchange("hello-java-exchange"
+        ,true, false);
+        amqpAdmin.declareExchange(directExchange);
+        log.info("Exchange创建成功");
     }
 
+    @Test
+    public void createQueue() {
+        Queue queue = new Queue("hello-java-queue", true, false, false);
+        amqpAdmin.declareQueue(queue);
+        log.info("队列[{}]创建成功", "hello-java-exchange");
+    }
+
+    @Test
+    public void createBinding() {
+        Binding binding = new Binding("hello-java-queue",
+                Binding.DestinationType.QUEUE, "hello-java-exchange", "hello.java",
+                null);
+        amqpAdmin.declareBinding(binding);
+        log.info("binding创建成功");
+    }
 }
