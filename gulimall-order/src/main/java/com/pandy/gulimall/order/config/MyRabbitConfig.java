94829434a93aaf5828860dd51a2c0d73eb754da8
Pandy <yangpandy@gmail.com>
2021-09-27 16:37:45 +0800

    rabbitmq

--- gulimall-order/src/main/java/com/pandy/gulimall/order/config/MyRabbitConfig.java
+++ gulimall-order/src/main/java/com/pandy/gulimall/order/config/MyRabbitConfig.java
@@ -1,10 +1,16 @@
 package com.pandy.gulimall.order.config;
 
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @Author Pandy
@@ -13,8 +19,27 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyRabbitConfig {
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @PostConstruct
+    public void initRabbitTemplate() {
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                System.out.println("Confirm..correlationData[" + correlationData +"]==>ack" + cause);
+            }
+        });
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                System.out.println("消息未送达制定队列时回调");
+            }
+        });
     }
 }
