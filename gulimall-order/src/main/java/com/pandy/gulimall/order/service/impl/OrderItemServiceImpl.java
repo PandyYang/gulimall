530f07558d7a426f1be270e268e858c837b6df8d
PandyYang <yangpandy@gmail.com>
2021-12-31 14:55:47 +0800

    rabbitlistener 和 rabbithandler说明

--- gulimall-order/src/main/java/com/pandy/gulimall/order/service/impl/OrderItemServiceImpl.java
+++ gulimall-order/src/main/java/com/pandy/gulimall/order/service/impl/OrderItemServiceImpl.java
@@ -10,13 +10,11 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.pandy.common.utils.PageUtils;
 import com.pandy.common.utils.Query;
-
 import com.pandy.gulimall.order.dao.OrderItemDao;
 import com.pandy.gulimall.order.entity.OrderItemEntity;
 import com.pandy.gulimall.order.service.OrderItemService;
