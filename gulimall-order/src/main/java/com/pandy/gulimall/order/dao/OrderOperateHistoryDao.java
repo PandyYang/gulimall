9d120dd7105d85e70c9aef36ce5152b02c783913
PandyYang <yangpandy@gmail.com>
2022-03-09 11:37:19 +0800

    update

--- gulimall-order/src/main/java/com/pandy/gulimall/order/dao/OrderOperateHistoryDao.java
+++ gulimall-order/src/main/java/com/pandy/gulimall/order/dao/OrderOperateHistoryDao.java
@@ -6,12 +6,12 @@ import org.apache.ibatis.annotations.Mapper;
 
 /**
  * 订单操作历史记录
- * 
+ *
  * @author Pandy
  * @email yangpandy@gmail.com
  * @date 2021-08-11 00:40:52
  */
 @Mapper
 public interface OrderOperateHistoryDao extends BaseMapper<OrderOperateHistoryEntity> {
-	
+
 }
