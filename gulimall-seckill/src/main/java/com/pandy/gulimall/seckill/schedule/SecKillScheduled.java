2652e1e7ea7c97519230822418489893d5302b6c
PandyYang <yangpandy@gmail.com>
2022-03-30 15:53:23 +0800

    update

--- gulimall-seckill/src/main/java/com/pandy/gulimall/seckill/schedule/SecKillScheduled.java
+++ gulimall-seckill/src/main/java/com/pandy/gulimall/seckill/schedule/SecKillScheduled.java
@@ -28,7 +28,13 @@ public class SecKillScheduled {
     @Async
     @Scheduled(cron = "5 * * * * ?")
     public void uploadSeckillSkuLatest3Days() {
-        //为避免分布式情况下多服务同时上架的情况，使用分布式锁
+
+        /**
+         * 分布式环境下，使用定时任务同时上架秒杀商品的时候，也需要加分布式锁。
+         * 但是每个机器上的时间应该是不一样的，这个怎么解决呢？
+         * 把redis的秒杀信息存入队列，只要任意一台机器在其秒杀区间内消费掉即可。
+         * 即便时间是不一样的，分布式锁只要确保在同一瞬间不会存在多台机器重复消费。
+         */
         RLock lock = redissonClient.getLock(upload_lock);
         try {
             lock.lock(10, TimeUnit.SECONDS);
