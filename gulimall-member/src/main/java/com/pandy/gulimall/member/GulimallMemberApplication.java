4751469775ace515b8ab6579a684229715278608
PandyYang <yangpandy@gmail.com>
2022-06-25 15:27:41 +0800

    add ribbon to load balance

--- gulimall-member/src/main/java/com/pandy/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/pandy/gulimall/member/GulimallMemberApplication.java
@@ -1,18 +1,12 @@
 package com.pandy.gulimall.member;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.stream.Collectors;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -21,5 +15,11 @@ public class GulimallMemberApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GulimallMemberApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
